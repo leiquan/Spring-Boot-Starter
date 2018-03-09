0ad26abee86b07d3ff82d3ac7cd8fa081fd899a3
leiquan <leiquan@live.com>
2018-03-10 03:11:53 +0800

    update

--- src/main/java/com/leiquan/controller/TestController.java
+++ src/main/java/com/leiquan/controller/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@RequestMapping("/show")
+	@RequestMapping("/")
 	public String show(){
 		return "Hello World";		
 	}
