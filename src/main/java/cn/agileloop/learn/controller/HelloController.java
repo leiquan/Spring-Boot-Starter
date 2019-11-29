329b0de56ebe103094b2b634b6f9625f088a00cb
leiquan <leiquan@live.com>
2019-11-30 01:48:29 +0800

    开始架构

--- src/main/java/cn/agileloop/learn/controller/HelloController.java
+++ src/main/java/cn/agileloop/learn/controller/HelloController.java
@@ -1,4 +1,4 @@
-package cn.agileloop.learn;
+package cn.agileloop.learn.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
